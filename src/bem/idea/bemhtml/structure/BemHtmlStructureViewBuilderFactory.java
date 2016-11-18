ffc1746ac3ec9b20b5e3a7e3e53e95e5e3b6ede8
Andrey Melikhov <melikhov@yamoney.ru>
2016-11-19 00:17:18 +0300

    let's begin

--- src/bem/idea/bemhtml/structure/BemHtmlStructureViewBuilderFactory.java
+++ src/bem/idea/bemhtml/structure/BemHtmlStructureViewBuilderFactory.java
@@ -5,8 +5,10 @@ import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class BemHtmlStructureViewBuilderFactory implements PsiStructureViewFactory {
 
@@ -14,7 +16,7 @@ public class BemHtmlStructureViewBuilderFactory implements PsiStructureViewFacto
         if (!(psiFile instanceof BemHtmlElement)) return null;
         return new TreeBasedStructureViewBuilder() {
             @NotNull
-            public StructureViewModel createStructureViewModel() {
+            public StructureViewModel createStructureViewModel(@Nullable Editor var1) {
                 return new BemHtmlStructureViewModel(psiFile);
             }
 
